75146837a27eda99edc6ac885af377ca2943732d
NguyenQuynhThu <letter.japan@gmail.com>
2019-07-24 18:26:28 +0700

    fibonacci - thay chua

--- Demo_C-/Demo_C-/fibonacci.cs
+++ Demo_C-/Demo_C-/fibonacci.cs
@@ -5,23 +5,29 @@ namespace Demo_C_
 {
     public class fibonacci
     {
-        public static void Main(String[] args)
+        //ham main chi dam nhan nhap-xuat cua chuong trinh
+        static void Main(String[] args)
         {
-         Console.WriteLine("Nhap so thu tu so fibonacci muon hien thi: ");
-         int n = Convert.ToInt32(Console.ReadLine());
-         int x1 = 0;
-         int x2 = 1;
-         int x = 1;
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("So fibonacci thu "+n+" la: "+Fibo(n));
 
-         for (int i = 3; i <= n; i++)
-         {
-             x = x1 + x2;
-             x1 = x2;
-             x2 = x;
-         }
-         
-         Console.WriteLine("So fibonacci thu "+n+ " la: " +x);
         }
-        
+
+        //tach biet cac chuc nang --> viet ra 1 ham khac
+        static int Fibo(int n)
+        {
+            if (n == 1 || n == 2) return 1;
+            if (n == 3) return 2;
+            int x1 = 1, x2 = 1, x3 = 2;
+            for (int i = 4; i <=n; i++)
+            {
+                x1 = x2;
+                x2 = x3;
+                x3 = x1 + x2;
+            }
+            return x3;
+
+        }
     }
 }
